37acc63eb8bf4e66b283347f7fa890cd178366a7
Henry Webel <heweb@dtu.dk>
2026-06-16 19:23:31 +0200

    :sparkles: Add basic enrichment analysis

---
+++ 3_enrichment_analysis.py
@@ -0,0 +1,180 @@
+# %% [markdown]
+# # Enrichment analysis
+
+# %%
+from collections import defaultdict
+from pathlib import Path
+
+import acore
+import acore.differential_regulation as ad
+import numpy as np
+import pandas as pd
+from acore.io.kegg import link_kegg_batch
+
+
+def parse_compound_pathway_mapping(raw_mapping: str) -> dict[str, list[str]]:
+    """Parse tab-delimited KEGG-style compound/pathway mappings into a dictionary."""
+    compound_to_pathways: defaultdict[str, list[str]] = defaultdict(list)
+
+    for line in raw_mapping.strip().strip("'").splitlines():
+        compound_id, pathway_id = line.split("\t", maxsplit=1)
+        compound_to_pathways[compound_id].append(pathway_id)
+
+    return dict(compound_to_pathways)
+
+
+compounds = {}
+
+
+# %% tags=["parameters"]
+fname_ms2query = "results_prepared/output_ms2query_Linked_data.tsv"
+fname_ancova = "results_prepared/ancova_results.csv"
+fname_pathways_map = "results_prepared/pathways_map.tsv"
+fname_inchikey_to_kegg = "results_prepared/inchikey_to_kegg.csv"
+
+
+# %%
+pathways_map = pd.read_csv(
+    fname_pathways_map, sep="\t", header=None, names=["pathway_id", "pathway_name"]
+)
+pathways_map.head()
+
+# %% [markdown]
+# exclude some generic pathways?
+
+# %%
+mask = pathways_map["pathway_name"].str.contains("pathways", case=False)
+pathways_map.loc[mask]
+
+# %% [markdown]
+# Can be downloaded from KEGG:
+# - https://rest.kegg.jp/link/compound/pathway
+
+
+# %%
+fname_annotations = Path(".") / "link_compound_pathway.tsv"
+annotations = pd.read_csv(
+    fname_annotations,
+    sep="\t",
+    header=None,
+    names=[
+        "pathway_id",
+        "compound_id",
+    ],
+)
+_ = annotations.insert(1, "Source", "KEGG")
+annotations["pathway_id"] = (
+    annotations["pathway_id"].str.strip().str.replace("path:", "")
+)
+annotations["compound_id"] = (
+    annotations["compound_id"].str.strip().str.replace("cpd:", "")
+)
+annotations.set_index("compound_id", inplace=True)
+annotations
+
+# %% [markdown]
+# ## Filtering pathways
+# filter some generic pathways?
+
+# %%
+annotations.groupby("pathway_id").size().sort_values(ascending=False)
+
+
+# %% [markdown]
+#  - [map01100](https://rest.kegg.jp/get/path:map01100/image)
+#  - [map01110](https://rest.kegg.jp/get/path:map01110/image)
+# -  [map01120](https://rest.kegg.jp/get/path:map01120/image)
+# -  [map01130](https://rest.kegg.jp/get/path:map01130/image)
+
+# %% [markdown]
+# annotations.groupby("pathway_id").size().sort_values(ascending=False).plot(
+#     kind="line", figsize=(10, 5), marker="."
+# )
+
+# %% markdown
+# - https://rest.kegg.jp/get/path:map00030+path:map00010
+
+# %%
+ms2query_results = pd.read_csv(fname_ms2query, index_col=0, sep="\t").drop_duplicates(
+    subset=["inchikey", "smiles"]
+)
+ms2query_results.head()
+
+# %%
+inchikey_to_kegg = pd.read_csv(fname_inchikey_to_kegg, index_col=0).astype({"id": str})
+inchikey_to_kegg
+
+# %%
+
+# %% [markdown]
+# - conf map [conf map of map00010](https://rest.kegg.jp/get/path:map00010/conf)
+#
+# ![path:map00010/image](https://rest.kegg.jp/get/path:map00010/image)
+
+
+# %%
+filter = ["path:map00010", "path:map00030", "path:map00052"]
+
+# %% [markdown]
+# ## Map features to MS2Query results
+#
+# - only needed as features identifies are based on xcms and ms2query results are based on
+#   metaboigniter, so the feature IDs don't match. If we use features from metaboigniter
+#   for the differential analysis, we could have avoided this step.
+#
+
+# %%
+diff_reg = pd.read_csv(fname_ancova, index_col=0)
+diff_reg.index = diff_reg.index.astype(str)
+diff_reg
+
+# %%
+regex_filter = "pval|padj|reject|FC"
+ids_found_inMS2 = inchikey_to_kegg['id'].unique().tolist()
+diff_reg.loc[ids_found_inMS2].filter(regex=regex_filter).sort_values('pvalue')
+
+# %%
+diff_reg.loc[ids_found_inMS2, "pvalue"] = 0.01
+diff_reg.loc[ids_found_inMS2].filter(regex=regex_filter).sort_values("pvalue")
+
+# %%
+inchikey_to_kegg #.loc[ids_found_inMS2]
+
+# %%
+rename_index = {
+    "9988206023938663754": "C12048",
+    "11438838801741346891": "C03194",  # C02962
+    "15780731469960021248": "C00193",
+}
+diff_reg = diff_reg.rename(index=rename_index)
+
+# %%
+diff_reg.loc[rename_index.values()].filter(regex=regex_filter).sort_values("pvalue")
+
+# %%
+annotations = annotations.rename_axis("identifier").reset_index()
+annotations
+
+# %% [markdown]
+# ## Enrichment analysis
+
+# %%
+ret = acore.enrichment_analysis.run_up_down_regulation_enrichment(
+    regulation_data=diff_reg.rename_axis("identifier").reset_index(),
+    annotation=annotations,
+    identifier="identifier",
+    annotation_col='pathway_id',
+    pval_col="pvalue",
+    min_detected_in_set=1,
+    lfc_cutoff=0.012,
+)
+ret
+
+# %%
+diff_reg.loc[rename_index.values()].filter(regex=regex_filter).sort_values("pvalue")
+
+# %% [markdown]
+# Why do we only see one compound?
+
+# %%
+annotations.loc[annotations.identifier.isin(inchikey_to_kegg.kegg_id)]
---
+++ 3_enrichment_analysis_fetch_kegg.py
@@ -0,0 +1,93 @@
+# %%
+from collections import defaultdict
+from pathlib import Path
+
+import pandas as pd
+import pubchempy as pcp
+from acore.io.kegg import lookup_cid_to_kegg_id
+
+
+def parse_compound_pathway_mapping(raw_mapping: str) -> dict[str, list[str]]:
+    """Parse tab-delimited KEGG-style compound/pathway mappings into a dictionary."""
+    compound_to_pathways: defaultdict[str, list[str]] = defaultdict(list)
+
+    for line in raw_mapping.strip().strip("'").splitlines():
+        compound_id, pathway_id = line.split("\t", maxsplit=1)
+        compound_to_pathways[compound_id].append(pathway_id)
+
+    return dict(compound_to_pathways)
+
+
+compounds = {}
+
+
+# %% tags=["parameters"]
+fname_ms2query = "results_prepared/output_ms2query_Linked_data.tsv"
+
+# %%
+ms2query_results = pd.read_csv(fname_ms2query, index_col=0, sep="\t").drop_duplicates(
+    subset=["inchikey", "smiles"]
+)
+ms2query_results.head()
+
+# %%
+to_lookup = ms2query_results[
+    [
+        "ms2query_model_prediction",
+        "precursor_mz_difference",
+        "precursor_mz_query_spectrum",
+        "precursor_mz_analog",
+        "inchikey",
+        "analog_compound_name",
+        "smiles",
+    ]
+].drop_duplicates(subset=["inchikey", "smiles"])
+to_lookup
+
+# %% [markdown]
+# Find PubChem CIDs for the InChIKeys
+
+# %%
+# inchikeys = to_lookup["inchikey"]
+# inchikeys")
+
+# %%
+for _inchikey in to_lookup.inchikey.unique():
+    if _inchikey in compounds:
+        continue
+    compounds[_inchikey] = pcp.get_compounds(_inchikey, namespace="inchikey")
+compounds
+
+# %% [markdown]
+# Then look up KEGG IDs for those CIDs
+
+# %%
+cids = [c.cid for pcp_list in compounds.values() for c in pcp_list if c.cid is not None]
+kegg_compounds = lookup_cid_to_kegg_id(cids)
+kegg_compounds
+
+
+# %%
+inchikey_to_kegg = []
+for inchikey, pcp_list in compounds.items():
+    for c in pcp_list:
+        if c.cid is not None and c.cid in kegg_compounds:
+            inchikey_to_kegg.append((inchikey, kegg_compounds[c.cid]))
+inchikey_to_kegg
+
+# %%
+inchikey_to_kegg = pd.DataFrame(inchikey_to_kegg, columns=["inchikey", "kegg_id"])
+inchikey_to_kegg
+
+# %% [markdown]
+# And finally find KEGG pathways for those KEGG IDs.
+# inchikey_to_kegg = inchikey_to_kegg.join(
+#     to_lookup.reset_index().set_index("inchikey"), on="inchikey"
+# )
+# inchikey_to_kegg
+
+# %%
+fname = "results_prepared/inchikey_to_kegg.csv"
+inchikey_to_kegg.to_csv(fname, index=False)
+
+# %%
